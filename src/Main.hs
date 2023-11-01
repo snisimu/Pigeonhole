fb6db43a60b0f2fc4bb242ce67017349fd8dec3b
Shinji Nishimura <snisimu@gmail.com>
2023-11-01 19:09:30 +0900

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -3,28 +3,35 @@
 
 module Main where
 
+import System.Process
+
 import Data.List (nub, elemIndex)
+
+import Base
+import AtMostK.Binary
 
 main :: IO ()
 main = do
   return ()
 
-type ScopeID = String
-
-data Var
-  = X Int
-  | Aux String
-  | Scope ScopeID Var
-   deriving (Eq, Show)
-
-type CNF = [Clause]
-type Clause = [Literal]
-type Literal = (Bool, Var)
-
-cnfToDIMACSnum :: CNF -> [[Int]]
-cnfToDIMACSnum cnf =
+cnfToDIMACS :: CNF -> IO ()
+cnfToDIMACS cnf = do
   let vars = nub $ concatMap (map snd) cnf
-  in  flip map cnf \clause -> 
+      nss = flip map cnf \clause -> 
         flip map clause \(b, v) -> case elemIndex v vars of
-          Just i -> (if b then 1 else -1) * i + 1
+          Just i -> (if b then 1 else -1) * (i + 1)
           Nothing -> error $ "literalToDIMACSnum: variable " ++ show v ++ " not found"
+      ls = flip map nss \ns -> unwords $ map show ns ++ ["0"]
+  -- print vars -- [debug]
+  -- print nss -- [debug]
+  writeFile "the.cnf" $ unlines ls
+  -- > cnfToDIMACS [[(True, X 1), (False, X 2)], [(True, X 1), (True, X 3)]]
+  -- > cnfToDIMACS $ binary id [X 1, X 2, X 3] 2
+  -- > wsl -- ./minisat ./the.cnf
+
+solve :: CNF -> IO ()
+solve cnf = do
+  cnfToDIMACS cnf
+  system "wsl -- ./minisat ./the.cnf"
+  return ()
+  -- > solve $ binary id (map ((,) True . X) [1..3]) 2 ++ [[(True, X 1)],[(True, X 2)]]
